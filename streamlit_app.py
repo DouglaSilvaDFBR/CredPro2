aeba918c141d101f43b95242a7f07fc38223772c
DouglaSilvaDFBR <41701233+DouglaSilvaDFBR@users.noreply.github.com>
2025-05-04 16:07:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,149 +3,65 @@ import pandas as pd
 import math
 from pathlib import Path
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Estilo com CSS personalizado para deixar parecido com a imagem
+st.markdown("""
+    <style>
+        .menu {
+            background-color: #d3d3d3;
+            padding: 10px;
+            width: 200px;
+            height: 500px;
+            float: left;
+        }
+        .info {
+            background-color: white;
+            border: 1px solid black;
+            padding: 10px;
+            margin-left: 210px;
+            height: 500px;
+        }
+        .title {
+            background-color: #d5edcc;
+            padding: 10px;
+            font-weight: bold;
+            font-size: 20px;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+# Título superior
+st.markdown('<div class="title">CREDPRO2</div>', unsafe_allow_html=True)
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+# Layout da página
+st.markdown('<div class="menu">', unsafe_allow_html=True)
+st.markdown("**MENU DE OPÇÕES**")
+menu = st.radio("", [
+    "NOVO CLIENTE",
+    "ACOMPANHAR VENDA",
+    "RELATORIO DE VENDA",
+    "ANEXAR DOCUMENTOS",
+    "CCA",
+    "CREDITO",
+    "SAIR"
+])
+st.markdown('</div>', unsafe_allow_html=True)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+st.markdown('<div class="info"><strong>INFORMAÇÕES</strong><br><br>', unsafe_allow_html=True)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Conteúdo baseado na opção escolhida
+if menu == "NOVO CLIENTE":
+    st.write("Formulário para cadastrar novo cliente.")
+elif menu == "ACOMPANHAR VENDA":
+    st.write("Painel de acompanhamento de vendas.")
+elif menu == "RELATORIO DE VENDA":
+    st.write("Relatórios disponíveis para análise.")
+elif menu == "ANEXAR DOCUMENTOS":
+    st.write("Upload de documentos aqui.")
+elif menu == "CCA":
+    st.write("Acesso ao CCA.")
+elif menu == "CREDITO":
+    st.write("Informações sobre crédito.")
+elif menu == "SAIR":
+    st.write("Você saiu do sistema.")
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.markdown('</div>', unsafe_allow_html=True)
